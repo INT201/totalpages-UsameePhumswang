919966f5fa2f52235105ae81f057c106661d744b
UsameePhumswang <88129589+UsameePhumswang@users.noreply.github.com>
2022-09-05 21:24:56 +0700

    test2

--- totalPages.js
+++ totalPages.js
@@ -3,7 +3,7 @@ const { template } = require('@babel/core')
 function totalPages(arrayItems, rowsPerPage) {
   if(arrayItems == null || arrayItems == undefined){
     return undefined
-  }else if(rowsPerPage == null || rowsPerPage == undefined){
+  }else if(rowsPerPage == null || rowsPerPage == undefined || rowsPerPage == 0){
     return 1
   }else {return Math.ceil(arrayItems.length/rowsPerPage)}
 }
